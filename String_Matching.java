9a0b078177a0d30c6d89a565f20713861e4700eb
john <69750882+johnboop@users.noreply.github.com>
2024-04-29 14:54:07 +0530

    Update String_Matching.java

--- String_Matching.java
+++ String_Matching.java
@@ -23,14 +23,45 @@ public class String_Matching {
         return -1;
     }
 
-    public static int Rabin_Karp(String t, String p, int n, int m){
-        return 0;
+    public static boolean check(char[] arrt, char[] arrp, int i) {
+        for (char c : arrp) {
+           if (c!=arrt[i]) {
+            return false;
+           }
+           i++;
+        }
+        return true;
+    }
+    public static String Rabin_Karp(String t, String p, int n, int m){
+        char[] arrt = t.toCharArray();
+        char[] arrp = p.toCharArray();
+        int p_value = 0;
+        for (int i = 0; i < arrp.length; i++) {
+            p_value+= arrp[i];
+        }
+        p_value = p_value % 13;
+        int t_value = 0;
+        int crt =0;
+        int j=0;
+        for (int i = 0; i < arrt.length; i++) {
+            if (i>=arrp.length) {
+                crt-=arrt[j];
+                j++;
+            }
+            crt+=arrt[i];
+            if (crt%13 == p_value && check(arrt, arrp, j) ) {
+                System.err.println("finded");
+            }
+        }
+
+        return "not finded";
     }
     public static void main(String[] args) {
-        String t = "abcabaabcabac";
+        String t = "abcababcabac";
         String p = "abaa";
         int t_len = t.length();
         int p_len = p.length();
         System.err.println(normal_string_matching(t, p, t_len, p_len));
+        System.err.println(Rabin_Karp(t, p, t_len, p_len));
     }
 }
